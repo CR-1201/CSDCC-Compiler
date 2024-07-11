113ee26352d9dcdd5c66d5ecea09fb36c17358e3
Conroy <email@sample.com>
2024-07-12 01:33:31 +0800

    解决了大部分bug

--- src/ast/Number.java
+++ src/ast/Number.java
@@ -14,11 +14,19 @@ public class Number extends Node{
 
     @Override
     public void buildIrTree() {
+        String content = token.getContent();
+        int radix = 10; // 默认为十进制
         if( token.getType() == Token.TokenType.DECCON || token.getType() == Token.TokenType.HEXCON || token.getType() == Token.TokenType.OCTCON){
-            int num = Integer.parseInt(token.getContent());
+            if (token.getType() == Token.TokenType.HEXCON) {
+                radix = 16;
+                content = content.substring(2);
+            } else if (token.getType() == Token.TokenType.OCTCON) {
+                radix = 8;
+            }
+            int num = Integer.parseInt(content, radix);
             valueUp = new ConstInt(num);
         } else {
-            float num = Float.parseFloat(token.getContent());
+            float num = Float.parseFloat(content);
             valueUp = new ConstFloat(num);
         }
     }

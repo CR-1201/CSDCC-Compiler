f3c069b1906a73d1033e3960f240f590d289f2e4
mengqiaoyuanhe <109514806+authormq@users.noreply.github.com>
2024-07-12 13:32:00 +0800

    补充一点东西

--- src/pass/analysis/CFG.java
+++ src/pass/analysis/CFG.java
@@ -1,4 +1,23 @@
 package pass.analysis;
 
-public class CFG {
+import ir.Function;
+import ir.Module;
+import pass.Pass;
+
+/**
+ * CFG: 控制流图，是每一个函数内部的，每一个函数内都有一个CFG
+ */
+public class CFG implements Pass {
+    private Module module = Module.getModule();
+    public void run() {
+        for (Function function : module.getFunctionsArray()) {
+            if (!function.getIsBuiltIn()) {
+                // 针对每一个函数去新建CFG
+
+            }
+        }
+    }
+    private void buildCFG(Function function) {
+        //
+    }
 }

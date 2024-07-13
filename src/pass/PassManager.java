378d16e34f15125280147f42a1c33e9fe737588a
mengqiaoyuanhe <109514806+authormq@users.noreply.github.com>
2024-07-13 13:18:51 +0800

    feat: 完成dom信息的构建

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -2,6 +2,7 @@ package pass;
 
 import ir.Module;
 import pass.analysis.CFG;
+import pass.analysis.Dom;
 
 import java.util.ArrayList;
 
@@ -11,6 +12,7 @@ public class PassManager {
 
     public void run() {
         passes.add(new CFG());
+        passes.add(new Dom());
         for (Pass pass : passes) {
             pass.run();
         }

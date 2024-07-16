af52a157e98a57fd19c0c464661f85dca78b8af8
mjorah7 <flynnlayton@126.com>
2024-07-16 21:31:07 +0800

    feat: 增加了sideEffect分析

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -4,6 +4,8 @@ import ir.Module;
 import pass.analysis.CFG;
 import pass.analysis.Dom;
 import pass.analysis.LoopAnalysis;
+import pass.analysis.SideEffect;
+import pass.transform.DeadCodeEmit;
 import pass.transform.Mem2reg;
 import pass.transform.UselessReturnEmit;
 
@@ -18,7 +20,9 @@ public class PassManager {
         passes.add(new Dom());
 //        passes.add(new LoopAnalysis());
         passes.add(new Mem2reg());
-//        passes.add(new UselessReturnEmit());
+        passes.add(new SideEffect());
+        passes.add(new DeadCodeEmit());
+        passes.add(new UselessReturnEmit());
         for (Pass pass : passes) {
             pass.run();
         }

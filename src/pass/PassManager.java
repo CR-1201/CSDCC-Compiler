4a81bc12b2478b869ba840efac21e4103576ff59
Conroy <email@sample.com>
2024-07-19 01:47:15 +0800

    fix: 不开死代码删除

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -27,7 +27,7 @@ public class PassManager {
         passes.add(new runSimplifyInst());
         passes.add(new SideEffect());
         passes.add(new UselessReturnEmit());
-        passes.add(new DeadCodeEmit());
+//        passes.add(new DeadCodeEmit());
 
         GVNGCMPass();
         for (Pass pass : passes) {

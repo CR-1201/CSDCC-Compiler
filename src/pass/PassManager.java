006a92cee265babeb8c7d3086bd3001726ad647b
Conroy <email@sample.com>
2024-07-19 00:52:15 +0800

    fix: 测试一下

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -20,11 +20,11 @@ public class PassManager {
         passes.add(new CFG());
         passes.add(new Dom());
         passes.add(new LoopAnalysis());
-        passes.add(new runSimplifyInst());
         passes.add(new GlobalValueLocalize());
         passes.add(new Mem2reg());
         passes.add(new SCCP());
         passes.add(new MergeRedundantBr());
+        passes.add(new runSimplifyInst());
         passes.add(new SideEffect());
         passes.add(new UselessReturnEmit());
         passes.add(new DeadCodeEmit());

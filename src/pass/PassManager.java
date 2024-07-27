6a8f1107990f30507956e5cceba183063482ac8f
mengqiaoyuanhe <109514806+authormq@users.noreply.github.com>
2024-07-27 23:24:19 +0800

    临时fix unroll，正常了

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -18,29 +18,32 @@ public class PassManager {
     private ArrayList<Pass> passes = new ArrayList<>();
 
     public void run() {
+        passes.add(new GepFuse());
         passes.add(new CFG());
         passes.add(new Dom());
         passes.add(new LoopAnalysis());
         passes.add(new GlobalValueLocalize());
         passes.add(new Mem2reg());
-        passes.add(new GepFuse());
+        passes.add(new SCCP());
+        passes.add(new CFG());
         passes.add(new InlineFunction());
         passes.add(new SCCP());
         passes.add(new SimplifyInst());
         passes.add(new MergeBlocks());
-//        passes.add(new SideEffect());
+        passes.add(new SideEffect());
+        passes.add(new DeadCodeEmit());
 ////        passes.add(new UselessReturnEmit());
         passes.add(new UselessStoreEmit());  // UselessStoreEmit 前面，一定要进行函数副作用的分析
         GVNGCMPass();
-////        passes.add(new CFG());
-////        passes.add(new Dom());
-////        passes.add(new GAVN());  // GAVN前需要最新的CFG和Dom, 放在GVN GCM后面较好
+//        passes.add(new CFG());
+//        passes.add(new Dom());
+//        passes.add(new GAVN());  // GAVN前需要最新的CFG和Dom, 放在GVN GCM后面较好
         passes.add(new MathOptimize());
         passes.add(new UselessPhiEmit());
         passes.add(new LCSSA());
         passes.add(new LoopUnroll());
-        passes.add(new UselessPhiEmit());
-        passes.add(new MergeBlocks());
+//        passes.add(new UselessPhiEmit());
+//        passes.add(new MergeBlocks());
 //        passes.add(new InstructionCleanUp());
         for (Pass pass : passes) {
             pass.run();

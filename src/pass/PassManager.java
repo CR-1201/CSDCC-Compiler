d36485d3bdde65bd9e65fa883b0e4bdf6e0bf460
Conroy <2399891646@qq.com>
2024-07-27 23:22:39 +0800

    Update PassManager.java

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -27,22 +27,22 @@ public class PassManager {
         passes.add(new GepFuse());
         // SCCP后可能出现没有value的phi
         passes.add(new SCCP());
-//        passes.add(new UselessPhiEmit());
+        passes.add(new UselessPhiEmit());
         passes.add(new SimplifyInst());
         passes.add(new MathOptimize());
 
         //FIXME SCCP后要加CFG?
-//        passes.add(new CFG());
-//        passes.add(new InlineFunction());
+        passes.add(new CFG());
+        passes.add(new InlineFunction());
 
-//        // SCCP后可能出现没有value的phi
+        // SCCP后可能出现没有value的phi
         passes.add(new SCCP());
 //        passes.add(new UselessPhiEmit());
         passes.add(new SimplifyInst());
         passes.add(new MathOptimize());
-//        passes.add(new DeadCodeEmit());
+////        passes.add(new DeadCodeEmit());
         passes.add(new MergeBlocks());
-
+//
         passes.add(new SideEffect());
 //        passes.add(new UselessReturnEmit());
         // UselessStoreEmit 前面，一定要进行函数副作用的分析
@@ -55,17 +55,16 @@ public class PassManager {
         passes.add(new LoopUnroll());
 //        passes.add(new UselessPhiEmit());
         passes.add(new MergeBlocks());
-//
-//
+
         passes.add(new CFG());
         passes.add(new Dom());
         passes.add(new GAVN());  // GAVN前需要最新的CFG和Dom, 放在GVN GCM后面较好
 //
-//        passes.add(new CSE());
-//
-//        // SCCP后可能出现没有value的phi
+        passes.add(new CSE());
+
+        // SCCP后可能出现没有value的phi
         passes.add(new SCCP());
-//        passes.add(new UselessPhiEmit());
+        passes.add(new UselessPhiEmit());
         passes.add(new SimplifyInst());
 //
         passes.add(new MathOptimize());

d17d2f76eb3a8672d04b065d6b35ffce1a3b8097
mjorah7 <flynnlayton@126.com>
2024-08-07 09:46:57 +0800

    <trivial>

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -58,8 +58,6 @@ public class PassManager {
         passes.add(new LoopFold());
         passes.add(new MergeBlocks());
         passes.add(new DeadCodeEmit());
-//        passes.add(new MemSetOptimize());
-
         passes.add(new GepSplit());
         BasicPass();
         passes.add(new SCCP());
@@ -73,10 +71,8 @@ public class PassManager {
         passes.add(new SimplifyInst());
         passes.add(new MathOptimize());
         passes.add(new DeadCodeEmit());
-
         passes.add(new SideEffect());
-        passes.add(new UselessStoreEmit());  // UselessStoreEmit 前面，一定要进行函数副作用的分析
-
+        passes.add(new UselessStoreEmit());
         passes.add(new CFG());
         passes.add(new Dom());
 

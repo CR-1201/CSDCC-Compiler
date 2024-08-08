8d72025129bf7cdbec0e786a3cc125731366cea3
Conroy <2399891646@qq.com>
2024-08-08 22:48:53 +0800

    fix: 测试一下CSE

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -35,58 +35,58 @@ public class PassManager {
         passes.add(new SimplifyInst());
         passes.add(new MathOptimize());
         passes.add(new CSE());
-//        passes.add(new CFG());
-//        passes.add(new TailRecursionElimination());
-//        passes.add(new InlineFunction());
-//        passes.add(new GlobalValueLocalize());
-//
-//        Mem2RegPass();
-//        passes.add(new SCCP());
-//        passes.add(new SimplifyInst());
-//        passes.add(new MathOptimize());
-//        passes.add(new MergeBlocks());
-//        passes.add(new SideEffect());
-//        passes.add(new DeadCodeEmit());
-//        passes.add(new UselessReturnEmit());
-//        passes.add(new ADCE());
-//        passes.add(new UselessStoreEmit());
-//
-//        BasicPass();
-//        passes.add(new CFG());
-//        passes.add(new Dom());
-//        passes.add(new GepFuse());
-//        passes.add(new LICM());
-//        passes.add(new LCSSA());
-//        passes.add(new LoopUnroll());
-//        passes.add(new LoopFold());
-//        passes.add(new MergeBlocks());
-//        passes.add(new DeadCodeEmit());
-////        passes.add(new MemSetOptimize());
-//
-//        passes.add(new LoopStrengthReduction());
-//        passes.add(new GepSplit());
-//
-//        BasicPass();
-//        passes.add(new SCCP());
-//        passes.add(new UselessPhiEmit());
-//        passes.add(new SimplifyInst());
-//        passes.add(new MathOptimize());
-//        BasicPass();
-//        passes.add(new Peephole());
-//        passes.add(new GepFuse());
-//        passes.add(new UselessArrayStoreEmit());
-//        BasicPass();
-//        passes.add(new SimplifyInst());
-//        passes.add(new MathOptimize());
-//        passes.add(new DeadCodeEmit());
-//
-//        passes.add(new SideEffect());
-//        passes.add(new UselessStoreEmit());  // UselessStoreEmit 前面，一定要进行函数副作用的分析
-//
-//        passes.add(new Peephole());
-//
-//        passes.add(new CFG());
-//        passes.add(new Dom());
+        passes.add(new CFG());
+        passes.add(new TailRecursionElimination());
+        passes.add(new InlineFunction());
+        passes.add(new GlobalValueLocalize());
+
+        Mem2RegPass();
+        passes.add(new SCCP());
+        passes.add(new SimplifyInst());
+        passes.add(new MathOptimize());
+        passes.add(new MergeBlocks());
+        passes.add(new SideEffect());
+        passes.add(new DeadCodeEmit());
+        passes.add(new UselessReturnEmit());
+        passes.add(new ADCE());
+        passes.add(new UselessStoreEmit());
+
+        BasicPass();
+        passes.add(new CFG());
+        passes.add(new Dom());
+        passes.add(new GepFuse());
+        passes.add(new LICM());
+        passes.add(new LCSSA());
+        passes.add(new LoopUnroll());
+        passes.add(new LoopFold());
+        passes.add(new MergeBlocks());
+        passes.add(new DeadCodeEmit());
+//        passes.add(new MemSetOptimize());
+
+        passes.add(new LoopStrengthReduction());
+        passes.add(new GepSplit());
+
+        BasicPass();
+        passes.add(new SCCP());
+        passes.add(new UselessPhiEmit());
+        passes.add(new SimplifyInst());
+        passes.add(new MathOptimize());
+        BasicPass();
+        passes.add(new Peephole());
+        passes.add(new GepFuse());
+        passes.add(new UselessArrayStoreEmit());
+        BasicPass();
+        passes.add(new SimplifyInst());
+        passes.add(new MathOptimize());
+        passes.add(new DeadCodeEmit());
+
+        passes.add(new SideEffect());
+        passes.add(new UselessStoreEmit());  // UselessStoreEmit 前面，一定要进行函数副作用的分析
+
+        passes.add(new Peephole());
+
+        passes.add(new CFG());
+        passes.add(new Dom());
 
         for (Pass pass : passes) {
             pass.run();
